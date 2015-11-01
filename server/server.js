618826196a52fc8e8874de123d5ccef5a663fedd
Augusto Claro <augustoclaro1@hotmail.com>
2015-11-01 20:06:54 -0200

    Server path fix

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-var path = "C:\\pessoais\\flappy";
+var path = require('path');
 var port = 8080;
 
-connect().use(serveStatic(path)).listen(port, function(){
-  console.log("Servidor rodando na porta " + port + ".");
+connect().use(serveStatic(path.join(__dirname, '..'))).listen(port, function(){
+  console.log('Servidor rodando na porta ' + port + '.');
 });
